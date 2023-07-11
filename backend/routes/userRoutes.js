9ed2c76c847fc9e35906ae3979fc4bd0a7979e4f
NalinYadav <nalinyadav106@gmail.com>
2023-07-11 15:35:51 +0530

    full app added

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+
+router.post("/", registerUser);
+router.post("/login", authUser);
+router.get("/", protect, allUsers);
+module.exports = router;
