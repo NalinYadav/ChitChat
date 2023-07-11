582001aac524e41dfe39e740e1b3dfd86df47422
NalinYadav <nalinyadav106@gmail.com>
2023-07-11 17:24:25 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chit-chat-ui.vercel.app",
     // credentials: true,
   },
 });
